07a3585a76413fff40feada8a0a6977fa9a78b11
Abdulrahman Shawky <hawk18922@gmail.com>
2025-12-29 18:37:08 +0200

    First commit: project structure nearly done

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Home from "./pages/Home/Home.jsx";
+import ProductDetails from "./pages/ProductDetails/ProductDetails.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/productDetails",
+        element: <ProductDetails />,
+    },
+]);
+
+createRoot(document.getElementById("root")).render(
+    <RouterProvider router={router} />
+);
